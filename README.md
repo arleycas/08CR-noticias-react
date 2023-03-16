95bb2aed08aeac7896152dc5d0975990b352366a
arleycas <eyhsoncastro@gmail.com>
2023-03-16 18:44:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+PROYECTO: Buscador de noticias con una API
+
+- React puro con vite
+- Uso de Framework de Interfaz MUI (Material UI) https://mui.com/
---
+++ src/context/NoticiasProvider.jsx
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { useState, useEffect, createContext } from 'react';
+
+const NoticiasContext = createContext();
+
+const NoticiasProvider = ({children}) => {
+
+  const [ categoria, setCategoria ] = useState('general');
+  const [ arrNoticias, setArrNoticias ] = useState([]);
+  const [ pagina, setPagina ] = useState(1);
+  const [ totalNoticias, setTotalNoticias ] = useState(0);
+  
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+      const apiKey = import.meta.env.VITE_API_KEY;
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${apiKey}`;
+
+      const { data } = await axios(url);
+
+      setArrNoticias(data.articles);
+      setTotalNoticias(data.totalResults)
+      setPagina(1)
+    }
+
+    consultarAPI()
+
+  }, [categoria]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+      const apiKey = import.meta.env.VITE_API_KEY;
+      const url = `https://newsapi.org/v2/top-headlines?country=us&page=${pagina}&category=${categoria}&apiKey=${apiKey}`;
+
+      const { data } = await axios(url);
+
+      setArrNoticias(data.articles);
+      setTotalNoticias(data.totalResults)
+    }
+
+    consultarAPI()
+
+  }, [pagina]);
+
+  const handleChangeCategoria = e => {
+    setCategoria(e.target.value)
+  }
+
+  const handleChangePagina = (e, valor) => {
+    setPagina(valor); // ya que no son inputs si no <li>
+  }
+
+  return (
+    <NoticiasContext.Provider
+      value={{
+        categoria,
+        handleChangeCategoria,
+        arrNoticias,
+        totalNoticias,
+        handleChangePagina,
+        pagina
+      }}>
+      {children}
+    </NoticiasContext.Provider>
+  )
+}
+
+export { NoticiasProvider }
+export default NoticiasContext;
